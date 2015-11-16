53c39edd443319cae5a45c76db67b7c44b29cdc8
Madestas <5515531@gmail.com>
2015-11-16 19:05:16 +0300

    Show contains qulity
    Show quality content insted qulity of container

--- src/haven/GItem.java
+++ src/haven/GItem.java
@@ -182,7 +182,7 @@ public class GItem extends AWidget implements ItemInfo.SpriteOwner, GSprite.Owne
 
 	private ItemQualityInfo itemQualityInfoCache = null;
 
-	public ItemQualityInfo getItemQualityInfo() {
+		public ItemQualityInfo getItemQualityInfo() {
 		if (itemQualityInfoCache == null) {
 			List<ItemInfo> itemInfoList = null;
 			try {
@@ -192,7 +192,20 @@ public class GItem extends AWidget implements ItemInfo.SpriteOwner, GSprite.Owne
 				return null;
 			}
 			itemQualityInfoCache = new ItemQualityInfo();
-			for (ItemInfo info : info()) {
+			for (ItemInfo info : itemInfoList) {
+				if (info instanceof ItemInfo.Contents && info != null) {
+					for (ItemInfo infoContents : ((ItemInfo.Contents) info).sub) {
+						if (infoContents.getClass().getSimpleName().equals("QBuff")) {
+							try {
+								String qname = (String) infoContents.getClass().getDeclaredField("name").get(infoContents);
+								Double qval = (Double) infoContents.getClass().getDeclaredField("q").get(infoContents);
+								itemQualityInfoCache.setByType(qname, qval);
+							} catch (Exception ex) {
+						}
+						}
+					}
+					break;
+				}
 				if (info.getClass().getSimpleName().equals("QBuff")) {
 					try {
 						String qname = (String) info.getClass().getDeclaredField("name").get(info);
